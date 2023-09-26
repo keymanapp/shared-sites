b4b2f21de91b9ed3ee9d4afe616308645d9349fc
Marc Durdin <marc@durdin.net>
2023-09-26 20:25:21 +1000

    chore: fix typos

--- bootstrap.inc.sh
+++ bootstrap.inc.sh
@@ -5,14 +5,14 @@ readonly BOOTSTRAP_BUILDER="$(dirname "$BOOTSTRAP")/builder.inc.sh"
 function _bootstrap_init() {
   # If builder.inc.sh is missing, download it
   if [[ ! -f "$BOOTSTRAP_BUILDER" ]]; then
-    curl -fs https://raw.githubusercontent.com/keymanapp/shared-sites/main/resources/builder.inc.sh > "$BOOTSTRAP_BUILDER" || (rm -f "$BOOTSTRAP_BUILDER" && exit 1))
+    curl -fs https://raw.githubusercontent.com/keymanapp/shared-sites/main/resources/builder.inc.sh > "$BOOTSTRAP_BUILDER" || (rm -f "$BOOTSTRAP_BUILDER" && exit 1)
   fi
 }
 
 
 function bootstrap_configure() {
   # Download latest version of bootstrap before running common configuration
-  curl -fs https://raw.githubusercontent.com/keymanapp/shared-sites/main/bootstrap.inc.sh > "$BOOTSTRAP.tmp" || (rm -f "$BOOTSTRAP.tmp" && exit 1))
+  curl -fs https://raw.githubusercontent.com/keymanapp/shared-sites/main/bootstrap.inc.sh > "$BOOTSTRAP.tmp" || (rm -f "$BOOTSTRAP.tmp" && exit 1)
   mv -f "$BOOTSTRAP.tmp" "$BOOTSTRAP"
 
   # With the new bootstrapper, we'll re-download builder.inc.sh
