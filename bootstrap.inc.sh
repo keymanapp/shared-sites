301ff2bfb42aeb7aa9a92f67eb0e7fd409c77d71
Marc Durdin <marc@durdin.net>
2023-09-26 21:18:56 +1000

    chore: Update bootstrap.inc.sh

--- bootstrap.inc.sh
+++ bootstrap.inc.sh
@@ -1,26 +1,24 @@
 # Shared infrastructure for websites
 
-readonly BOOTSTRAP_BUILDER="$(dirname "$BOOTSTRAP")/builder.inc.sh"
+BOOTSTRAP_BUILDER="$(dirname "$BOOTSTRAP")/builder.inc.sh"
 
 function _bootstrap_init() {
-  # If builder.inc.sh is missing, download it
+  # If builder.inc.sh is missing, download both it and bootstrap once more
   if [[ ! -f "$BOOTSTRAP_BUILDER" ]]; then
-    curl -fs https://raw.githubusercontent.com/keymanapp/shared-sites/main/resources/builder.inc.sh > "$BOOTSTRAP_BUILDER" || (rm -f "$BOOTSTRAP_BUILDER" && exit 1)
+    curl -fs https://raw.githubusercontent.com/keymanapp/shared-sites/main/bootstrap.inc.sh -o "$BOOTSTRAP"
+    curl -fs https://raw.githubusercontent.com/keymanapp/shared-sites/main/resources/builder.inc.sh -o "$BOOTSTRAP_BUILDER"
   fi
 }
 
 
 function bootstrap_configure() {
-  # Download latest version of bootstrap before running common configuration
-  curl -fs https://raw.githubusercontent.com/keymanapp/shared-sites/main/bootstrap.inc.sh > "$BOOTSTRAP.tmp" || (rm -f "$BOOTSTRAP.tmp" && exit 1)
-  mv -f "$BOOTSTRAP.tmp" "$BOOTSTRAP"
-
-  # With the new bootstrapper, we'll re-download builder.inc.sh
+  # Re-download both bootstrap and builder.inc.sh
   rm -f "$BOOTSTRAP_BUILDER"
-
-  . "$BOOTSTRAP"
-
   _bootstrap_init
+
+  # Load the new bootstrap before doing additional downloads
+  source "$BOOTSTRAP"
+
   _bootstrap_configure_common
 }
 
