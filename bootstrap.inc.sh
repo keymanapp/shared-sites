b8e12d26a1751fd1556e14d09d27eede005b891a
Marc Durdin <marc@durdin.net>
2023-09-27 06:16:04 +0700

    chore: tidy up bootstrap.inc.sh

--- bootstrap.inc.sh
+++ bootstrap.inc.sh
@@ -29,10 +29,6 @@ fi
 # Define globals (if not already defined)
 #
 
-if [[ -z ${BOOTSTRAP_BUILDER+x} ]]; then
-  readonly BOOTSTRAP_BUILDER="$(dirname "$BOOTSTRAP")/builder.inc.sh"
-fi
-
 if [[ -z ${BOOTSTRAP_ROOT+x} ]]; then
   readonly BOOTSTRAP_ROOT="$(dirname "$BOOTSTRAP")/.."
 fi
@@ -43,7 +39,12 @@ fi
 function _bootstrap_download() {
   local remote_file="$1"
   local local_file="$2"
+  _bootstrap_echo "  Downloading $remote_file"
   curl -fs "https://raw.githubusercontent.com/keymanapp/shared-sites/main/$remote_file" -o "$local_file"
+}
+
+fnuction _bootstrap_echo() {
+  echo "[bootstrap] $*"
 }
 
 #
@@ -52,33 +53,22 @@ function _bootstrap_download() {
 # once by the prolog, and again in _bootstrap_init, but as this is a low-cost
 # operation, it makes the build script prolog cleaner to do it this way.
 #
-function _bootstrap_init() {
-  # If builder.inc.sh is missing, download both it and bootstrap once more
-  if [[ ! -f "$BOOTSTRAP_BUILDER" ]]; then
-    # Note that builder.inc.sh is a special case; it could be part of
-    # _common but we are using it as our signal for whether the repo needs
-    # additional files
-    _bootstrap_download bootstrap.inc.sh         "$BOOTSTRAP"
-    _bootstrap_download resources/builder.inc.sh "$BOOTSTRAP_BUILDER"
-
-    # Load the new bootstrap before doing additional downloads. The bootstrap
-    # include script has been designed to cope with being run multiple times;
-    # this ensures that the latest version of _bootstrap_configure_common is
-    # run.
-    source "$BOOTSTRAP"
-
-    _bootstrap_configure_common
-  fi
-}
-
-#
-# Update shared files -- call this on `build.sh configure`
+# This will be called on first run on a clean repo, and should also be called
+# on `build.sh configure`.
 #
 function bootstrap_configure() {
-  # Deleting builder.inc.sh will force a re-download of bootstrap.inc.sh
-  # and builder.inc.sh, and everything else
-  rm -f "$BOOTSTRAP_BUILDER"
-  _bootstrap_init
+  _bootstrap_echo "Bootstrap starting"
+  _bootstrap_download bootstrap.inc.sh "$BOOTSTRAP"
+
+  # Load the new bootstrap before doing additional downloads. The bootstrap
+  # include script has been designed to cope with being run multiple times;
+  # this ensures that the latest version of _bootstrap_configure_common is
+  # run.
+  source "$BOOTSTRAP"
+
+  _bootstrap_configure_common
+
+  _bootstrap_echo "Bootstrap complete"
 }
 
 #
@@ -87,6 +77,7 @@ function bootstrap_configure() {
 function _bootstrap_configure_common() {
   local BOOTSTRAP_COMMON="$BOOTSTRAP_ROOT/_common"
   local COMMON_FILES=(
+    builder.inc.sh
     docker.inc.sh
     keyman-local-ports.inc.sh
     JsonApiFailure.php
@@ -96,22 +87,28 @@ function _bootstrap_configure_common() {
   )
   local common_file=
 
-  echo "Downloading _common files"
+  _bootstrap_echo "Downloading _common files"
   rm -rf "$BOOTSTRAP_COMMON"
   mkdir -p "$BOOTSTRAP_COMMON"
 
   for common_file in "${COMMON_FILES[@]}"; do
-    echo "  Downloading $common_file..."
     _bootstrap_download "_common/$common_file" "$BOOTSTRAP_COMMON/$common_file"
   done
 
-  echo "All _common files downloaded"
+  _bootstrap_echo "All _common files downloaded"
 }
 
-# Test if resources need to be downloaded, in particular builder.inc.sh
-_bootstrap_init
+# Test if resources need to be downloaded
+if [[ ! -f "$BOOTSTRAP" ]]; then
+  _bootstrap_echo "Bootstrap required"
+  bootstrap_configure
+fi
 
 # Finally, we need to run builder.inc.sh, if it hasn't already been run
 if [[ -z ${THIS_SCRIPT_PATH+x} ]]; then
-  source "$BOOTSTRAP_BUILDER"
+  source "$(dirname "$THIS_SCRIPT")/_common/builder.inc.sh"
+
+  # Bootstrapped scripts always run from their own folder (probably $REPO_ROOT),
+  # but only do this on first-run, not if re-sourced with bootstrap_configure
+  cd "$THIS_SCRIPT_PATH"
 fi
