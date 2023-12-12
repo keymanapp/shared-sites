e0a491042e4c6772adc116d4dd24752c400fa84e
Darcy Wong <darcy_wong@sil.org>
2023-12-13 05:42:12 +0700

    Add KeymanSiteEnvironment to _bootstrap_configure_common

--- _common/KeymanSiteEnvironment.php
+++ _common/KeymanSiteEnvironment.php
@@ -3,17 +3,6 @@
 
   namespace Keyman\Site\Common;
 
-  class HelpSiteEnvironment extends KeymanSiteEnvironment {
-    public static
-      // help.keyman.com
-      $KEYMANHOSTS_TIER = '',
-      $KEYMAN_COM_PROXY_PORT = '';
-
-    public static function Init() {
-      KeymanSiteEnvironment::_Init(get_class());
-    }
-  }
-
   class KeymanSiteEnvironment {
 
     public static
@@ -43,4 +32,4 @@
 // HelpSiteEnvironment::Debug('Keyman\Site\Common\HelpSiteEnvironment');
 // echo HelpSiteEnvironment::$api_keyman_com_github_repo;
 
-HelpSiteEnvironment::Init();
+//HelpSiteEnvironment::Init();
