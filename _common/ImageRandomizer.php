31e763ae7a8f500dd173a26964782adeeaf7c386
Meng-Heng <havmengheng111@gmail.com>
2024-08-27 11:16:55 +0700

    adjusts code and image folder

--- _common/ImageRandomizer.php
+++ _common/ImageRandomizer.php
@@ -3,18 +3,17 @@
 namespace Keyman\Site\Common;
 
 class ImageRandomizer {
-    
-    static function randomizer($folder) { 
-        if(!empty($_SERVER['DOCUMENT_ROOT']) && !empty($folder)) {
-          $imgDir = $_SERVER['DOCUMENT_ROOT'] . '/cdn/dev/' . $folder;
-          $img = glob($imgDir . '*.{png,jpg,jpeg,gif}', GLOB_BRACE);
-          if(!empty($img)) {
-            shuffle($img);
-            return str_replace($_SERVER['DOCUMENT_ROOT'], '',  $img[0]);
-          }
-        }
-        return "Not found";
+  static function randomizer() { 
+    if(!empty($_SERVER['DOCUMENT_ROOT'])) {
+      $imgDir = $_SERVER['DOCUMENT_ROOT'] . '/_common/' . 'assets/sil-logos-2024/';
+      $img = glob($imgDir . '*.{png,jpg,jpeg,gif}', GLOB_BRACE);
+      if(!empty($img)) {
+        shuffle($img);
+        return str_replace($_SERVER['DOCUMENT_ROOT'], '',  $img[0]);
       }
+    }
+    return "Not found";
+  }
 }
 
 ?>
