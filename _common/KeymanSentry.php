3c39994a148873fb966e1eb5d2e4af458faec606
Marc Durdin <marc@durdin.net>
2025-02-18 14:04:22 +0700

    chore: don't use autoloader

--- _common/KeymanSentry.php
+++ _common/KeymanSentry.php
@@ -3,7 +3,8 @@
 
   namespace Keyman\Site\Common;
 
-  use \Keyman\Site\Common\KeymanHosts;
+  // Don't use autoloader here because we may be pre-autoloader setup
+  require_once __DIR__ . '/KeymanHosts.php';
 
   class KeymanSentry {
     static function init($dsn) {
