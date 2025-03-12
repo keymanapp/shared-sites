bf36f1efc0507da14a9f1f52465acd299c4f7a8b
Marc Durdin <marc@durdin.net>
2025-03-12 15:05:56 +0700

    feat: move sentry init javascript to common

--- _common/KeymanSentry.php
+++ _common/KeymanSentry.php
@@ -5,6 +5,7 @@
 
   // Don't use autoloader here because we may be pre-autoloader setup
   require_once __DIR__ . '/KeymanHosts.php';
+  require_once __DIR__ . '/Assets.php';
 
   class KeymanSentry {
     static function init($dsn) {
@@ -12,5 +13,40 @@
         'dsn' => $dsn,
         'environment' => KeymanHosts::Instance()->TierName()
       ]);
+    }
+
+    static function GetBrowserHtml($dsn) {
+
+      $asset = Assets::Get('js/sentry.js');
+      $tier = KeymanHosts::Instance()->TierName();
+
+      return <<<END
+        <script
+          src="https://js.sentry-cdn.com/bba22972ad6b4c2ab03a056f549cc23d.min.js"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://browser.sentry-cdn.com/9.1.0/bundle.tracing.min.js"
+          integrity="sha384-MCeGoX8VPkitB3OcF9YprViry6xHPhBleDzXdwCqUvHJdrf7g0DjOGvrhIzpsyKp"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://browser.sentry-cdn.com/9.1.0/captureconsole.min.js"
+          integrity="sha384-gkHY/HxnL+vrTN/Dn6S9siimRwqogMXpX4AetFSsf6X2LMQFsuXQGvIw7h2qWCt+"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://browser.sentry-cdn.com/9.1.0/httpclient.min.js"
+          integrity="sha384-ZsomH91NyAZy+YSYhJcpL3sSDFlkL310CJnpKNqL9KerB92RvfsH9tXRa2youKLM"
+          crossorigin="anonymous"
+        ></script>
+        <script>
+          const sentryEnvironment = {
+            dsn: '$dsn',
+            tier: '$tier',
+          }
+        </script>
+        <script src="$asset"></script>
+  END;
     }
   }
