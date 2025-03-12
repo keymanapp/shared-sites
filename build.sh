eb9a320850db02afcd21e9fabb2e39c4d475cbc8
Marc Durdin <marc@durdin.net>
2025-03-12 12:40:14 +0700

    feat: cdn support for assets
    Adds sha1sum of files in the assets folder and makes a copy so that
    these files can be used in place of the bare filenames, avoiding cache
    issues. Future possibilities of sharing these files.

--- build.sh
+++ build.sh
@@ -4,4 +4,38 @@ set -eu
 readonly THIS_SCRIPT="$(readlink -f "${BASH_SOURCE[0]}")"
 readonly THIS_SCRIPT_PATH="$(dirname "$THIS_SCRIPT")"
 
-find "$THIS_SCRIPT_PATH/_common" -type f '!' '-name' 'README.md' -printf '%P\n' > .bootstrap-registry
+cd "$THIS_SCRIPT_PATH"
+
+build_file_list() {
+  local regtemp="$(mktemp)"
+  local filename
+
+
+  rm -f .bootstrap-registry
+  rm -rf _common/cdn
+
+  find _common -type f '!' -name README.md -printf '%P\n' > "$regtemp"
+
+  while read filename; do
+    local base="$(basename "$filename")"
+    local dir="$(dirname "$filename")"
+
+    if [[ "$dir" =~ ^assets/ ]]; then
+      local file="${base%.*}"
+      local ext="${base##*.}"
+
+      sha1=$(sha1sum "_common/$filename" | cut -d' ' -f 1 -)
+      local sha1filename="$dir/$file.$sha1.$ext"
+
+      mkdir -p "_common/cdn/$dir"
+      cp "_common/$filename" "_common/cdn/$sha1filename"
+      echo "$filename $sha1filename" >> .bootstrap-registry
+    else
+      echo "$filename" >> .bootstrap-registry
+    fi
+  done < "$regtemp"
+
+  rm -f "$regtemp"
+}
+
+build_file_list
