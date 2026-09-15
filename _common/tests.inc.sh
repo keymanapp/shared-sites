cea78b5fc254df63e9a0f1837cf8749843e64d05
Marc Durdin <marc@durdin.net>
2026-09-15 09:47:04 +0200

    fix: need bash not sh for tests.container.sh
    Test-bot: skip

--- _common/tests.inc.sh
+++ _common/tests.inc.sh
@@ -9,8 +9,10 @@
 # path
 if [[ $OSTYPE =~ msys|cygwin ]]; then
   TEST_CONTAINER_ROOT_PATH=//var/www/html
+  TEST_CONTAINER_BASH=//bin/bash
 else
   TEST_CONTAINER_ROOT_PATH=/var/www/html
+  TEST_CONTAINER_BASH=/bin/bash
 fi
 
 # Record the start time for unit tests for later log review
@@ -93,17 +95,17 @@ function do_test_print_link_report() {
 #
 function do_test_print_container_error_logs() {
   local CONTAINER="$1"
-  docker exec "${CONTAINER}" sh $TEST_CONTAINER_ROOT_PATH/_common/tests.container.sh report
+  docker exec "${CONTAINER}" $TEST_CONTAINER_BASH $TEST_CONTAINER_ROOT_PATH/_common/tests.container.sh report
 }
 
 function do_test_links_setup() {
   local CONTAINER="$1"
-  docker exec "${CONTAINER}" sh $TEST_CONTAINER_ROOT_PATH/_common/tests.container.sh setup
+  docker exec "${CONTAINER}" $TEST_CONTAINER_BASH $TEST_CONTAINER_ROOT_PATH/_common/tests.container.sh setup
   docker kill "${CONTAINER}" --signal="USR1"
 }
 
 function do_test_links_cleanup() {
   local CONTAINER="$1"
-  docker exec "${CONTAINER}" sh $TEST_CONTAINER_ROOT_PATH/_common/tests.container.sh cleanup
+  docker exec "${CONTAINER}" $TEST_CONTAINER_BASH $TEST_CONTAINER_ROOT_PATH/_common/tests.container.sh cleanup
   docker kill "${CONTAINER}" --signal="USR1"
 }
