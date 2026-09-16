1e90b9bae8add14bf80b21b6795a0e9bcf301c3e
Marc Durdin <marc@durdin.net>
2026-09-16 11:21:31 +0700

    maint: add status.keyman.com to dockerized sites list (#91)
    Test-bot: skip

--- sites.inc.sh
+++ sites.inc.sh
@@ -2,6 +2,6 @@
 # Keyman is copyright (C) SIL Global. MIT License.
 
 # Site repositories that run on Docker
-export sites=(api.keyman.com help.keyman.com keyman.com keymanweb.com s.keyman.com website-local-proxy)
+export sites=(api.keyman.com help.keyman.com keyman.com keymanweb.com s.keyman.com status.keyman.com website-local-proxy)
 export repositories=("${sites[@]/#/keymanapp/}")
 export site_targets=("${sites[@]/#/:}")
